d529abf7ae4d752821ce52b0fdc82880361e0ffc
Hanh Nguyen <hanh.nguyen2@sotatek.com>
2021-03-13 16:41:00 +0700

    feat: init nest and build api structure

---
+++ code/api-nest/src/database/entities/Product.entity.ts
@@ -0,0 +1,43 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate } from 'typeorm';
+import { nowInMillis } from '../../shared/Utils';
+
+@Entity('product')
+export class Product {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'int' })
+  id: number;
+
+  @Column({ name: 'name', type: 'varchar', length: 80, nullable: false })
+  name: string;
+
+  @Column({ name: 'slug', type: 'varchar', length: 191, nullable: false, unique: true })
+  slug: string;
+
+  @Column({ name: 'description', type: 'varchar', length: 255, nullable: false })
+  description: string;
+
+  @Column({ name: 'type', type: 'int', nullable: false })
+  type: number;
+
+  @Column({ name: 'gender', type: 'int', nullable: false })
+  role: string;
+
+  @Column({ name: 'image', type: 'varchar', length: 255, nullable: false })
+  image: string;
+
+  @Column({ name: 'created_at', type: 'bigint', nullable: true })
+  createdAt: number;
+
+  @Column({ name: 'updated_at', type: 'bigint', nullable: true })
+  updatedAt: number;
+
+  @BeforeInsert()
+  public updateCreateDates() {
+    this.createdAt = nowInMillis();
+    this.updatedAt = nowInMillis();
+  }
+
+  @BeforeUpdate()
+  public updateUpdateDates() {
+    this.updatedAt = nowInMillis();
+  }
+}
---
+++ code/api-nest/src/database/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './User.entity';
+export * from './Product.entity';
+export * from './Subscription.entity';
+export * from './Crate.entity';
